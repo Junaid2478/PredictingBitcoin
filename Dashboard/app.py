33dbdabb35728c08e671bbec466951f0c89f3ab6
Junaid2478 <79209011+Junaid2478@users.noreply.github.com>
2021-08-14 17:01:41 +0100

    dropdown works

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -3,7 +3,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
+from dash.dependencies import Output, Input
 
+from models.KNN_model import run_knn
 from models.LSTM_model import run_lstm
 
 
@@ -21,6 +23,7 @@ import plotly.tools as tls
 import pandas as pd
 
 from models.LinearRegression_model import run_linear_regression
+from models.RandomForest_model import run_random_forest
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -31,17 +34,13 @@ colors = {
     'text': '#7FDBFF'
 }
 
-fig = run_linear_regression()
-fig = tls.mpl_to_plotly(fig)
 
+
+model_names=['KNN', 'LSTM', 'LinearRegression','RandomForest']
 # print(df)
 # fig = px.scatter(df, x="Dates", y="Predicted")
 
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color=colors['text']
-)
+
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
@@ -58,23 +57,50 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     }),
 
     dcc.Dropdown(
-        id='demo-dropdown',
-        options=[
-            {'label': 'New York City', 'value': 'NYC'},
-            {'label': 'Montreal', 'value': 'MTL'},
-            {'label': 'San Francisco', 'value': 'SF'}
-        ],
-        placeholder="Select a model",
-        value='NYC'
+        id='xaxis-column',
+        options=[{'label': i, 'value': i} for i in model_names],
+        value='Fertility rate, total (births per woman)'
     ),
     html.Div(id='dd-output-container'),
 
-
-    dcc.Graph(
-        id='example-graph-2',
-        figure=fig
-    )
+    dcc.Graph(id='indicator-graphic')
 ])
+
+
+@app.callback(
+    Output('indicator-graphic', 'figure'),
+    Input('xaxis-column', 'value'))
+
+def update_graph(model_name,):
+    if model_name=='KNN':
+        fig = run_knn()
+    elif model_name=='LinearRegression':
+        fig = run_linear_regression()
+    elif model_name=='LSTM':
+        fig = run_lstm()
+    elif model_name=='RandomForest':
+        fig = run_random_forest()
+    fig = tls.mpl_to_plotly(fig)
+    fig.update_layout(
+        plot_bgcolor=colors['background'],
+        paper_bgcolor=colors['background'],
+        font_color=colors['text']
+    )
+    # dff = df[df['Year'] == year_value]
+    #
+    # fig = px.scatter(x=dff[dff['Indicator Name'] == xaxis_column_name]['Value'],
+    #                  y=dff[dff['Indicator Name'] == yaxis_column_name]['Value'],
+    #                  hover_name=dff[dff['Indicator Name'] == yaxis_column_name]['Country Name'])
+    #
+    # fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+    #
+    # fig.update_xaxes(title=xaxis_column_name,
+    #                  type='linear' if xaxis_type == 'Linear' else 'log')
+    #
+    # fig.update_yaxes(title=yaxis_column_name,
+    #                  type='linear' if yaxis_type == 'Linear' else 'log')
+
+    return fig
 
 app.run_server(debug=True)
 
